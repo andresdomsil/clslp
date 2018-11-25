84b99800a9564ae8c8a521686a6d6b85b59a6c7a
Antonio <ing.antonio.gutierrezgarcia@outlook.com>
2018-11-25 12:39:36 -0600

    1

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('base')
+
+@section('container')
+<section class="pages container">
+		<div class="page page-about">
+ //pendiente
+		</div>
+	</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,13 @@
 Route::get('/', function(){ return view('welcome');         } )->name('home');
 
 Route::get('admin', function(){ return view('admin.dashboard'); } );
+
+Route::get('about', 'PagesController@about')->name('pages.about');
+Route::get('contacto', 'PagesController@contact')->name('pages.contact');
+Route::post('contacto/send', 'PagesController@store')->name('pages.contact.send');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function(){
+
+       Route::get('users', 'UsersController@index')->name('admin.users.index');
+
+});
