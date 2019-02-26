ca7e95d2f2fc33df618bc08c64e65486850cbe49
GGA <martin.rodriguez3004@gmail.com>
2019-02-26 07:37:29 -0600

    GGA - Certificaciones(Rutas)

--- routes/web.php
+++ routes/web.php
@@ -129,3 +129,13 @@ Route::get('/actdash/{id?}','ActController@show');
 //Empresas
 //Nueva Empresa
 Route::get('/newempresa','EmpresasControllerr@create');
+
+//Certificaciones
+//Lista de Certificaciones
+Route::get('listcertificacion', 'CertificacionesController@index');
+Route::get('certificaciones', 'CertificacionesController@create');
+Route::post('certificaciones', 'CertificacionesController@store');
+Route::get('certificaciones/{id?}', 'CertificacionesController@show');
+Route::get('certificaciones/{id?}/edit', 'CertificacionesController@edit');
+Route::post('certificaciones/{id?}/edit', 'CertificacionesController@update');
+Route::get('certificaciones/{id?}/delete', 'CertificacionesController@destroy');
