d7542abff6b0c4ab15ac963b742b09b481a3c739
GGA <martin.rodriguez3004@gmail.com>
2019-02-26 08:06:20 -0600

    GGA - Cert (1)

---
+++ resources/views/certificaciones/show.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.layout')
+@section('content')
+	<div class="container col-md-8 col-md-offset-2">
+		<div class="well well bs-component">
+			<div class="content">
+				<h2 class="header">{!! $certificacion->nombre !!}</h2>
+				<p><strong>Ultima Actualizaci&oacute;n</strong>: {!! $certificacion->updated_at !!}</p>
+			</div>
+			<a href="{!! action('CertificacionesController@edit', $certificacion->id) !!}" class="btn btn-info">Editar</a>
+			<a herf="{!! action('CertificacionesController@destroy', $certificacion->id) !!}" class="btn btn-default">Borrar</a>
+		</div>
+	</div>
+@stop
